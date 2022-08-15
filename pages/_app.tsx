93789c82ea65fb36da0375c631e4130010d4f053
Matthew Wong <matthew.chohin@gmail.com>
2022-08-16 00:18:48 +0800

    chore: add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+          components: {
+            Container: {
+              defaultProps: {
+                sizes: {
+                  xs: 540,
+                  sm: 720,
+                  md: 960,
+                  lg: 1140,
+                  xl: 1320,
+                },
+              },
+            },
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import { Container } from "@mantine/core";
+import { createGetInitialProps } from "@mantine/next";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+import aboutStyles from "../styles/About.module.css";
+
+const getInitialProps = createGetInitialProps();
 
 const Home: NextPage = () => {
   const [offsetY, setOffsetY] = useState(0);
@@ -29,7 +34,9 @@ const Home: NextPage = () => {
       <section className={styles.parallax}>
         <div
           className={styles.parallaxBackground}
-          style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+          style={{
+            transform: `translateY(-${offsetY}px)`,
+          }}
         />
 
         <div
@@ -40,8 +47,8 @@ const Home: NextPage = () => {
         </div>
         <div className={styles.content}>{renderContent()}</div>
       </section>
-      <section className={styles.parallax}>
-        <div className={styles.parallaxBackground} />
+      <section className={aboutStyles.about}>
+        <Container>About</Container>
       </section>
     </>
   );
