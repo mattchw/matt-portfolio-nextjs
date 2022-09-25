b133246166f90f5573762ee9d08b14b18c19dda3
Matthew Wong <matthew.chohin@gmail.com>
2022-09-25 11:18:16 +0800

    feat(constant): add quotes

---
+++ constants/quotes.ts
@@ -0,0 +1,25 @@
+export const quotes = [
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    cite: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    cite: "Walt Disney",
+  },
+  {
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    cite: "Eleanor Roosevelt",
+  },
+  {
+    quote:
+      "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+    cite: "James Cameron",
+  },
+  {
+    quote: "He who has a why to live can bear almost any how.",
+    cite: "Friedrich Nietzsche",
+  },
+];
