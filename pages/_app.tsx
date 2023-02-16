a8b4f067af50ee15ff3659d994eacc143a1f1d1a
Matthew Wong <matthew.chohin@gmail.com>
2023-02-16 19:22:09 +0000

    style: add custom container breakdown widths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -73,6 +73,19 @@ export default function App(props: AppProps) {
           withNormalizeCSS
           theme={{
             /** Put your mantine theme override here */
+            components: {
+              Container: {
+                defaultProps: {
+                  sizes: {
+                    xs: 540,
+                    sm: 720,
+                    md: 960,
+                    lg: 1140,
+                    xl: 1320,
+                  },
+                },
+              },
+            },
             colorScheme,
           }}
         >
